5e2181e46f58719974f37fd8b64a0fbd6c26fd68
ZHUW099 <Disney201912>
2020-10-06 09:50:55 +0800

    update txt

--- src/main/java/com/joyce/SpringReactiveApplication.java
+++ src/main/java/com/joyce/SpringReactiveApplication.java
@@ -5,8 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class SpringReactiveApplication {
 	private static final Logger logger = LoggerFactory.getLogger(SpringReactiveApplication.class);
 
---
+++ src/main/java/com/joyce/TomcatApplication.java
@@ -0,0 +1,28 @@
+package com.joyce;
+
+import org.apache.catalina.LifecycleException;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+/**
+ * @author: Joyce Zhu
+ * @date: 2020/10/4
+ */
+@SpringBootApplication
+@EnableWebFlux
+public class TomcatApplication {
+
+    public static void main(String[] args) throws LifecycleException {
+//        HttpHandler handler = ...
+//        Servlet servlet = new TomcatHttpHandlerAdapter(handler);
+//
+//        Tomcat server = new Tomcat();
+//        File base = new File(System.getProperty("java.io.tmpdir"));
+//        Context rootContext = server.addContext("", base.getAbsolutePath());
+//        Tomcat.addServlet(rootContext, "main", servlet);
+//        rootContext.addServletMappingDecoded("/", "main");
+////        server.setHost("127.0.0.1");
+//        server.setPort(9999);
+//        server.start();
+    }
+}
